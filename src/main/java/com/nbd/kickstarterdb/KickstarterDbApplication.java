eaa2e901454fbdd21a832a12aef4efcb900fb893
arturradiuk <arturradiuk@gmail.com>
2020-11-27 15:47:48 +0200

    The template of the spring boot app has been created.

---
+++ src/main/java/com/nbd/kickstarterdb/KickstarterDbApplication.java
@@ -0,0 +1,13 @@
+package com.nbd.kickstarterdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickstarterDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickstarterDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nbd/kickstarterdb/KickstarterDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbd.kickstarterdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickstarterDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
