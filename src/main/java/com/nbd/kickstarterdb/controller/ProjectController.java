e9ca444af887a0143f1ea23eadfcc96994ef6376
arturradiuk <arturradiuk@gmail.com>
2020-11-27 15:54:51 +0200

    Get methods has been implemented.

--- src/main/java/com/nbd/kickstarterdb/controller/ProjectController.java
+++ src/main/java/com/nbd/kickstarterdb/controller/ProjectController.java
@@ -1,8 +1,15 @@
 package com.nbd.kickstarterdb.controller;
 
+import com.nbd.kickstarterdb.model.Project;
 import com.nbd.kickstarterdb.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 @CrossOrigin(origins = "http://localhost:8081")
@@ -12,5 +19,40 @@ public class ProjectController {
 
     @Autowired
     ProjectRepository projectRepository;
+
+    @GetMapping("/projects/get")
+    public ResponseEntity<List<Project>> getAllProjects(@RequestParam(required = false) String name) {
+        try {
+            List<Project> projects = new ArrayList<Project>();
+            if (name == null) {
+                projectRepository.findAll().forEach(projects::add);
+            } else
+                projectRepository.findByNameContaining(name).forEach(projects::add);
+
+            if (projects.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(projects, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/projects/get/{ID}")
+    public ResponseEntity<Project> getProjectByID(@PathVariable("ID") Integer ID) {
+
+        Optional<Project> project = projectRepository.findById(ID);
+        System.out.println(project.isPresent());
+
+        if (project.isPresent()) {
+            return new ResponseEntity<>(project.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
 
 }
