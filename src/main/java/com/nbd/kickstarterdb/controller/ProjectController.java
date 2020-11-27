7f78121aead8bc4c36943215578cf320921202a9
arturradiuk <arturradiuk@gmail.com>
2020-11-27 16:47:58 +0200

    Post method and put method has been added, but have not been tested.

--- src/main/java/com/nbd/kickstarterdb/controller/ProjectController.java
+++ src/main/java/com/nbd/kickstarterdb/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.nbd.kickstarterdb.controller;
 
+import com.datastax.driver.core.utils.UUIDs;
 import com.nbd.kickstarterdb.model.Project;
 import com.nbd.kickstarterdb.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 
 @CrossOrigin(origins = "http://localhost:8081")
@@ -53,6 +55,70 @@ public class ProjectController {
         }
 
     }
+
+    @DeleteMapping("/projects/delete")
+    public ResponseEntity<Project> deleteAllProjects() {
+        try {
+            projectRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/projects/delete/{ID}")
+    public ResponseEntity<Project> deleteProjectByID(@PathVariable("ID") Integer ID) {
+        try {
+            projectRepository.deleteById(ID);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+    @PostMapping("/projects/post") // todo need to be tested
+    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+        try {
+            Project _project = projectRepository.save(new Project(project));
+            return new ResponseEntity<>(_project, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+//    @PostMapping("/projects/post/{ID}") // todo need to be implemented
+
+//    @PutMapping("/projects/post/") // todo need to be implemented
+
+    @PutMapping("/projects/put/{ID}") // todo need to be tested
+    public ResponseEntity<Project> updateProject(@PathVariable("ID") Integer ID, @RequestBody Project project) {
+        Optional<Project> projectData = projectRepository.findById(ID);
+
+        if (projectData.isPresent()) {
+            Project _project = projectData.get();
+            _project.setName(project.getName());
+            _project.setCategory(project.getCategory());
+            _project.setMain_category(project.getMain_category());
+            _project.setCurrency(project.getCurrency());
+            _project.setDeadline(project.getDeadline());
+            _project.setGoal(project.getGoal());
+            _project.setLaunched(project.getLaunched());
+            _project.setPledged(project.getPledged());
+            _project.setState(project.getState());
+            _project.setBackers(project.getBackers());
+            _project.setCountry(project.getCountry());
+            _project.setUsd_pledged(project.getUsd_pledged());
+            _project.setUsd_pledged_real(project.getUsd_pledged_real());
+            _project.setUsd_goal_real(project.getUsd_goal_real());
+            return new ResponseEntity<>(projectRepository.save(_project), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 
 
 }
