9251c0c411b3174f7e8966ec7d2723f16170156f
aradiuk <arturradiuk@gmail.com>
2021-01-11 22:55:12 +0200

    file with jemeter tests has been added

---
+++ Timer.java
@@ -0,0 +1,7 @@
+public class Timer {
+    public static void main(String args[])
+            throws InterruptedException {
+        Thread.sleep(15000);
+        System.out.println("hello world\n");
+    }
+}
