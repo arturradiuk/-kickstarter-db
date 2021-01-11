e4325d9187efc52bd35604cc3966defe6ee7e093
aradiuk <arturradiuk@gmail.com>
2021-01-11 22:56:24 +0200

    redundant file removing

--- Timer.java
+++
@@ -1,7 +0,0 @@
-public class Timer {
-    public static void main(String args[])
-            throws InterruptedException {
-        Thread.sleep(15000);
-        System.out.println("hello world\n");
-    }
-}
